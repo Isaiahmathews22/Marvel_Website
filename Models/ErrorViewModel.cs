6f07552f3d84bdf656f42c0b51afa0b189b4a0dd
isaia <isaia@_ISAIAH_PC>
2023-02-21 10:46:05 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Marvel_Website.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
